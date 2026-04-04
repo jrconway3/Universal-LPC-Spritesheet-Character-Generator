08c19d88228a1b82d5e9a9b67207b1d425d5d2c3
IoriBranford <ioribranford@gmail.com>
2026-04-03 19:02:07 -0700

    Include items' selection names in custom area items
    So exportSplitItemAnimations can use them to construct correct filenames

--- sources/canvas/renderer.js
+++ sources/canvas/renderer.js
@@ -132,6 +132,7 @@ export async function renderCharacter(
 
           customAnimationItems.push({
             itemId,
+            name: selection.name,
             variant,
             recolors,
             spritePath,
@@ -184,6 +185,7 @@ export async function renderCharacter(
 
           itemsToDraw.push({
             itemId,
+            name: selection.name,
             variant,
             recolors,
             spritePath,
@@ -345,6 +347,8 @@ export async function renderCharacter(
               spritePath: item.spritePath,
               itemId: item.itemId,
               animation: customAnimName,
+              variant: item.variant,
+              name: item.name,
             });
           }
         }
@@ -363,6 +367,7 @@ export async function renderCharacter(
                 needsRecolor: item.needsRecolor,
                 recolors: item.recolors,
                 variant: item.variant,
+                name: item.name,
               });
             }
           }
