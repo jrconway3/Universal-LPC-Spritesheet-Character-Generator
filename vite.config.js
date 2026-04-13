b22ed135440793c9a0e0ed28ebaa2a33cf3dac75
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-04-13 07:31:52 -0400

    optimize chunk size

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,24 @@ export default defineConfig({
       input: {
         main: "index.html",
       },
+      output: {
+        codeSplitting: {
+          minSize: 20000,
+          maxSize: 200000,
+          minModuleSize: 20000,
+          maxModuleSize: 200000,
+          maxInitialChunkSize: 200000,
+          maxAsyncChunkSize: 200000,
+        },
+        groups: [
+          {
+            name: "vendor",
+            test: /node_modules/,
+          },
+        ],
+      },
     },
+    target: "esnext",
   },
   css: {
     target: false,
