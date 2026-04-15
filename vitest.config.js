b7ddc0858c9330b9ad52d94f3a7f28176b349f09
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-04-15 06:36:14 -0400

    fix ci: home.spec.js was not excluded

--- vitest.config.js
+++ vitest.config.js
@@ -85,7 +85,7 @@ export default mergeConfig(
       },
       setupFiles: ["tests/vitest-setup.js"],
       include: ["tests/**/*_spec.js"],
-      exclude: ["tests/visual/**/*_spec.js", "tests/node/**/*_spec.js"],
+      exclude: ["tests/visual/**", "tests/node/**"],
       testTimeout: 10_000,
     },
   }),
